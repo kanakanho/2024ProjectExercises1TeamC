ed890fa9d1bfa7ca7257cc82f5f7f4fca67a46df
ayu <umaminokiami@gmail.com>
2024-04-23 23:55:35 +0900

    add-icon

---
+++ index.js
@@ -0,0 +1,18 @@
+
+//ページの遷移だけ
+const Tetris = () => {
+    console.log("Tetris");
+    window.location = "https://sushida.net/";
+}
+const Quiz = () => {
+    console.log("Quiz");
+    window.location = "https://sushida.net/"
+}
+const what = () => {
+    console.log("Tetris");
+    window.location = "https://sushida.net/";
+}
+const What = () => {
+    console.log("Quiz");
+    window.location = "https://sushida.net/"
+}
