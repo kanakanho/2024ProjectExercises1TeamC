a4a56e6f6014516fb6b91a2688162db2c8a3276c
kanakanho <kahoshibakichi@gmail.com>
2024-05-14 15:39:19 +0900

    fix: レビューに従って修正、不要な要素の削除

--- quiz/src/main.js
+++ quiz/src/main.js
@@ -1,12 +1,12 @@
 function changeStage(stageNumber) {
-    if (stageNumber == -1) {
+    if (stageNumber === -1) {
         setTimeout(() => {
             location.href = `/quiz/clear.html`;
         }, 1300);
     } else {
         stageNumber++;
         console.log(`/quiz/quiz${stageNumber}.html`);
-        // 2s wait
+        // wait
         setTimeout(() => {
             location.href = `/quiz/quiz${stageNumber}.html`;
         }, 1300);
