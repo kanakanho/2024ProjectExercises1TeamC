a2d7466a433ff384a2b6f24108d79abcefdc4b1a
kanakanho <kahoshibakichi@gmail.com>
2024-05-21 17:43:26 +0900

    fix: 画面が正しく遷移しない問題を修正

--- quiz/src/main.js
+++ quiz/src/main.js
@@ -1,13 +1,14 @@
 function changeStage(stageNumber) {
     if (stageNumber === -1) {
         setTimeout(() => {
-            location.href = `./quiz/clear.html`;
+            location.href = `./clear.html`;
         }, 1300);
     } else {
         stageNumber++;
+        console.log(`./quiz${stageNumber}.html`);
         // wait
         setTimeout(() => {
-            location.href = `./quiz/quiz${stageNumber}.html`;
+            location.href = `./quiz${stageNumber}.html`;
         }, 1300);
     }
 }
