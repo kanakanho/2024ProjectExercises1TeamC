47a324b183278d2b1b5498e806ea61f570f16af3
ayu <umaminokiami@gmail.com>
2024-05-22 21:31:30 +0900

    change

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const moveGameWindow = (gameName) => {
   //ページの遷移だけ
   if (gameName == "game1") {
     console.log("game1");
-    window.location = "https://sushida.net/";
+    window.location = "./quiz/";
   } else if (gameName == "game2") {
     console.log("game2");
-    window.location =
-      "https://ja.wikipedia.org/wiki/%E3%83%9A%E3%83%B3%E3%82%AE%E3%83%B3";
+    window.location = "./castle-quiz/top.html";
   } else if (gameName == "game3") {
     console.log("game3");
-    window.location = "https://www.aeonpet.com/topics/pet-column_50.html";
+    window.location = "./kanji/";
   } else if (gameName == "game4") {
     console.log("game4");
-    window.location = "https://www.akkodis.co.jp/candidate/insight/column_30";
+    window.location = "./dog_quiz/0-menu/";
   } else {
     console.error("ゲームが見つからず遷移できませんでした");
   }
